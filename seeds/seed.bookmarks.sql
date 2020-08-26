10ac8d48db6dfe42f91c069974d8ecb6583bc8b5
Amy Amrani <vesamrani@gmail.com>
2020-08-26 10:43:34 -0400

    clean up

--- seeds/seed.bookmarks.sql
+++ seeds/seed.bookmarks.sql
@@ -1,12 +1,12 @@
 INSERT INTO bookmarks (title, url, description, rating)
 VALUES
-  ('Google', 'http://wwww.google.com', 'Search Engine', '5'),
-  ('Twitter', 'http://wwww.twitter.com', 'Social Media', '4'),
+  ('Google', 'http://google.com', 'Search Engine', '5'),
+  ('Twitter', 'http://twitter.com', 'Social Media', '4'),
   ('Pinterest', 'http://pinterest.com', 'Social Media Board', '3'),
   ('StumbleUpon', 'http://stumbleupon.com', 'Social Media Toolbar', '3'),
   ('Dribble', 'https://twitter.com/dribbble', 'For designers', '2'),
   ('Pocket', 'https://getpocket.com', 'Saves your interests', '4'),
   ('Digg', 'http://digg.com', 'Find new content and organise it through your profile', '1'),
-  ('Reddit', 'http://www.reddit.com', 'Promote your own content', '5'),
+  ('Reddit', 'http://reddit.com', 'Promote your own content', '5'),
   ('Slashdot', 'http://slashdot.com', 'User-submitted new stories', '1'),
-  ('Instagram', 'http://wwww.instagram.com', 'Search Media Posting', '2');
+  ('Instagram', 'http://instagram.com', 'Search Media Posting', '2');
